bf34ec6570bb0b8d2a9cd5287919bf75ccc8d4bb
Capdoo <mazher_2014@hotmail.com>
2022-02-06 21:47:21 -0500

    first commit

---
+++ src/test/java/com/paginacion/app/PaginacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paginacion.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
